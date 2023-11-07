98afadbae3d377317a2f094aebcc8ba00e6dfb7f
dxnielemotta <danielemottarocha@gmail.com>
2023-11-07 17:54:34 -0300

    variables rename and add function setup

--- sketch.js
+++ sketch.js
@@ -1,13 +1,29 @@
-let grama
-let coelho
-let cenoura
-let tamanho = 64
+let grass
+let bunny
+let carrot
+let size = 64
 
 let andarX = 0
 let andarY = 0
 
 var random
-var randomIntervalo
+var randomInterval
 
 var positionX = 0
 var positionY = 0
+
+let button
+
+function setup() {
+  createCanvas(576, 576);
+  //se 512 dividido por um numero dentro do intervalo de 0-512 
+  //for diferente de 0, essa posição não pode ser utilizada
+  random = Math.floor(Math.random() * 8) 
+  randomInterval = random * 64
+  
+
+
+  grass = loadImage('grass.png')
+  bunny = loadImage('bunny.png')
+  carrot = loadImage('cenoura.png')
+}
